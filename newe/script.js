60772482c989b59ac7ed0e0503f74f1786cb2bc2
=Biswaranjan <biswaranjanb627@gmail.com>
2025-04-03 23:34:58 +0530

    03/04/2025

---
+++ newe/script.js
@@ -0,0 +1,42 @@
+
+
+// function changeText(){
+//     let fpara=document.getElementById('fpara');
+//     fpara.textContent="biswaranjan biswal";
+// }
+
+// let fpara=document.getElementById('fpara');
+
+// fpara.addEventListener('click',changeText);
+
+
+// let anchorElement=document.getElementById("fanchor");
+
+// anchorElement.addEventListener('click',function(event){
+//     event.preventDefault();
+//     anchorElement.textContent="click done bhai";
+// });
+
+
+// let fpara=document.getElementById('fpara');
+// fpara.addEventListener('click',function(event){
+//    fpara.textContent="new"; 
+// });
+
+// let paras=document.querySelectorAll('p');
+
+// for(let i=0;i<paras.length;i++){
+//     let para=paras[i];
+//     para.addEventListener('click',function(){
+//         alert("you have clicked on the "+(i)+"number para");
+//     })
+// }
+
+function alertPara(event){
+    alert("you have clicked on the number para"+event.target.textContent);
+}
+let div=document.getElementById('div');
+
+document.addEventListener('click',alertPara);
+
+   
