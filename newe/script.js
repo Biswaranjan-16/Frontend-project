99d547e9390dd080ff6b1a554653ca8f988e1902
=Biswaranjan <biswaranjanb627@gmail.com>
2025-04-10 23:34:56 +0530

    10/04/2025

--- newe/script.js
+++ newe/script.js
@@ -32,11 +32,24 @@
 //     })
 // }
 
-function alertPara(event){
-    alert("you have clicked on the number para"+event.target.textContent);
-}
-let div=document.getElementById('div');
+// function alertPara(event){
+//     alert("you have clicked on the number para"+event.target.textContent);
+// }
+// let div=document.getElementById('div');
 
-document.addEventListener('click',alertPara);
+// document.addEventListener('click',alertPara);
 
+
+// let fpara=document.getElementById('fpara');
+// fpara.addEventListener('click',function(event){
+// alert("hii");
+// });
    
+
+async function getdata() {
+   setTimeout(() => {
+    console.log("i am biswaranjan biswal");
+   }, 3000);
+}
+
+getdata();
